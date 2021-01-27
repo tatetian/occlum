4abab83d946347acbb266799ec6f7574ce2f9b51
Tate, Hongliang Tian <tatetian@gmail.com>
2021-01-27 15:01:06 +0000

    Fix compiling problems

--- src/libos/src/fs/channel.rs
+++ src/libos/src/fs/channel.rs
@@ -274,7 +274,7 @@ impl<I: Copy> Producer<I> {
             return Ok(0);
         }
 
-    /*
+        /*
         waiter_loop!(
             {
                 let mut rb_producer = self.inner.lock().unwrap();
@@ -421,41 +421,41 @@ impl<I: Copy> Consumer<I> {
         }
 
         todo!("make function async")
-    /*
-        waiter_loop!(
-            {
-                let mut rb_consumer = self.inner.lock().unwrap();
-                if self.is_self_shutdown() {
-                    return_errno!(EPIPE, "this endpoint has been shutdown");
-                }
-
-                let mut total_count = 0;
-                for items in item_slices.iter_mut() {
-                    let count = rb_consumer.pop_slice(items);
-                    total_count += count;
-                    if count < items.len() {
-                        break;
-                    } else {
-                        continue;
+        /*
+            waiter_loop!(
+                {
+                    let mut rb_consumer = self.inner.lock().unwrap();
+                    if self.is_self_shutdown() {
+                        return_errno!(EPIPE, "this endpoint has been shutdown");
                     }
-                }
 
-                if total_count > 0 {
-                    drop(rb_consumer);
-                    self.trigger_peer_events(&IoEvents::OUT);
-                    return Ok(total_count);
-                };
+                    let mut total_count = 0;
+                    for items in item_slices.iter_mut() {
+                        let count = rb_consumer.pop_slice(items);
+                        total_count += count;
+                        if count < items.len() {
+                            break;
+                        } else {
+                            continue;
+                        }
+                    }
 
-                if self.is_peer_shutdown() {
-                    return Ok(0);
-                }
-                if self.is_nonblocking() {
-                    return_errno!(EAGAIN, "try again later");
-                }
-            },
-            self.observer.waiter_queue()
-        )
-    */
+                    if total_count > 0 {
+                        drop(rb_consumer);
+                        self.trigger_peer_events(&IoEvents::OUT);
+                        return Ok(total_count);
+                    };
+
+                    if self.is_peer_shutdown() {
+                        return Ok(0);
+                    }
+                    if self.is_nonblocking() {
+                        return_errno!(EAGAIN, "try again later");
+                    }
+                },
+                self.observer.waiter_queue()
+            )
+        */
     }
 }
 
